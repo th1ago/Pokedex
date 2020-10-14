063e017de11bd5ede4bf8f52e51c475dda42e055
Thiago Murakami <thiago_murakami@hotmail.com>
2020-10-14 15:22:57 -0300

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="App-topbar">
+            <center>
+                <h1>Pokedex</h1>
+                <h6>Thiago Murakami</h6>
+                <a href="https://pokeapi.co/">https://pokeapi.co/</a>
+                <p></p>
+            </center>
+        </div>
+    )
+};
+
+export default Header
